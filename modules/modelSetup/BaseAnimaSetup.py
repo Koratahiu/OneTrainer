4e7112400c5d8cfd2d558d405a3edd2f39292f12
Koratahiu~ <haidermontage191@gmail.com>
2026-06-09 16:17:55 +0300

    add

--- modules/modelSetup/BaseAnimaSetup.py
+++ modules/modelSetup/BaseAnimaSetup.py
@@ -104,6 +104,11 @@ class BaseAnimaSetup(
                 text_encoder_dropout_probability=config.text_encoder.dropout_probability if not deterministic else None,
             )
 
+            if config.cep_gamma > 0 and not deterministic:
+                text_encoder_output = self._apply_conditional_embedding_perturbation(
+                    text_encoder_output, config.cep_gamma, generator
+                )
+
             latent_image = batch['latent_image']
             scaled_latent_image = model.scale_latents(latent_image)
             latent_noise = self._create_noise(scaled_latent_image, config, generator)
@@ -116,6 +121,7 @@ class BaseAnimaSetup(
                 scaled_latent_image.shape[0],
                 config,
                 shift=shift if config.dynamic_timestep_shifting else config.timestep_shift,
+                sigmas=model.noise_scheduler.sigmas,
             )
 
             scaled_noisy_latent_image, sigma = self._add_noise_discrete(
